6279ad9ae86d57af33e8bd73039bd957381d9606
wanglonghong <wanglonghong4467@gmail.com>
2021-03-13 02:47:44 +0800

    update contract address for bsc mainnet

--- src/constants/combustPriceContracts.ts
+++ src/constants/combustPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {combustAddress: string, busdAddress: string, lpAddress:string} = {
-    combustAddress: '0x5d3a2568c4079eeada9aaa6cbfc5eeea2bfcfa79',
-    busdAddress: '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee',
-    lpAddress: '0x37fcda70840a1e72c9e38f4ee410d12117b38c6a'
+    combustAddress: '0xbE3F4046ca72252F91A6A598F81398065C50059f',
+    busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    lpAddress: '0xddb537aadf973144e792cded0897c059e8921d51'
   }
   
   export default priceContracts
